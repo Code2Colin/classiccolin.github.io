9b88fd7eb404c90c9e03c67a1f02d8d59a0e021d
classiccolin <classiccolin@gmail.com>
2022-07-26 14:48:42 -0700

    add temperature data

--- sketch.js
+++ sketch.js
@@ -1,34 +1,46 @@
-let value = 0 ;
-let back = 200 ;
-let c;
+// let value = 0 ;
+// let back = 200 ;
+// let c;
+
+getData();
+
+async function getData() {
+    const myRequest = new Request('./test.csv');
+    const response = await fetch(myRequest,{'mode':"same-orgins"});
+    const data = await response.text();
+    console.log(data);
+
+}
+// papa.parse('test.csv')
 
 
-function setup(){
-    // createCanvas(400,400) ;
-    // let canvas = createCanvas(200,200) ;
-    let canvas = createCanvas(200,200) ;
-    canvas.parent('canvasforHTML');
+
+// function setup(){
+//     // createCanvas(400,400) ;
+//     // let canvas = createCanvas(200,200) ;
+//     let canvas = createCanvas(200,200) ;
+//     canvas.parent('canvasforHTML');
     
-    // let img = createImage(200,200) ;
-    // image(img,0,0) ;
-    background(back) ;
-}
-
-
-function draw(){
-    // background(100) ;
-    if (mouseIsPressed === true) {
-        strokeWeight(10) ;
-    } else {
-        strokeWeight(0) ;
-    }
-    // point(mouseX,mouseY) ;
-    line(mouseX,mouseY,pmouseX,pmouseY) ;
-    // mousePressed() ;
-}
-
-// function mousePressed() {
-//     c = get() ;
-//     image(c, width/2, height/2) ;
+//     // let img = createImage(200,200) ;
+//     // image(img,0,0) ;
+//     background(back) ;
 // }
+
+
+// function draw(){
+//     // background(100) ;
+//     if (mouseIsPressed === true) {
+//         strokeWeight(10) ;
+//     } else {
+//         strokeWeight(0) ;
+//     }
+//     // point(mouseX,mouseY) ;
+//     line(mouseX,mouseY,pmouseX,pmouseY) ;
+//     // mousePressed() ;
+// }
+
+// // function mousePressed() {
+// //     c = get() ;
+// //     image(c, width/2, height/2) ;
+// // }
 
