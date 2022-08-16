223e08b254d7948eb158bf1f65e69171140785f4
classiccolin <classiccolin@gmail.com>
2022-08-16 15:33:22 -0700

    Added Graph
    added graph with star and button to increase the points

---
+++ index.js
@@ -0,0 +1,75 @@
+
+// getData();
+// const xlabels = [];
+// const ylabels = [];
+
+// async function getData() {
+//     const response = await fetch('./ZonAnnTs+dSST.csv');
+//     const data = await response.text();
+//     const rows = data.split("\n").slice(1);
+
+//     rows.forEach(elt => {
+//         const row = elt.split(',');
+//         const year = row[0];
+//         const temp = row[1];
+//         xlabels.push(year);
+//         ylabels.push(temp);
+//     });
+// }
+
+// console.log(xlabels);
+// console.log(ylabels);
+
+// const ctx = document.getElementById('mychart').getContext('2d');
+// const myChart = new CharacterData(ctx, {
+//     type:'line',
+//     data:{
+//         labels: xlabels,
+//         datasets: [
+//             {
+//                 data:ylabels
+//             }
+//         ]
+//     } 
+// }
+// )
+// papa.parse('test.csv')
+
+// const Chart = require('chart.js');
+// import Chart from 'chart.js/auto';
+
+// const ctx = document.getElementById("myChart");
+// const myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
--- sketch.js
+++ sketch.js
@@ -2,42 +2,77 @@
 // let back = 200 ;
 // let c;
 
-getData();
+// getData();
+// const xlabels = [];
+// const ylabels = [];
 
-async function getData() {
-    const myRequest = new Request('https://raw.githubusercontent.com/classiccolin/classiccolin.github.io/main/ZonAnnTs+dSST.csv');
-    const response = await fetch(myRequest);
-    const data = await response.text();
-    console.log(data);
+// async function getData() {
+//     const response = await fetch('./ZonAnnTs+dSST.csv');
+//     const data = await response.text();
+//     const rows = data.split("\n").slice(1);
 
-    const rows = data.split("\n").slice(1);
-    rows.forEach(elt => {
-        const row = elt.split(',');
-        const year = row[0];
-        const temp = row[1];
-        xlabels.push(year);
-        ylabels.push(temp);
-        console.log(year);
-        console.log(temp);
-    });
-}
-const xlabels = [];
-const ylabels = [];
-const ctx = document.getElementById('mychart').getContext('2d');
-const myChart = new CharacterData(ctx, {
-    type:'line',
-    data:{
-        labels: xlabels,
-        datasets: [
-            {
-                data:ylabels
-            }
-        ]
-    }
-}
-)
+//     rows.forEach(elt => {
+//         const row = elt.split(',');
+//         const year = row[0];
+//         const temp = row[1];
+//         xlabels.push(year);
+//         ylabels.push(temp);
+//     });
+// }
+
+// console.log(xlabels);
+// console.log(ylabels);
+
+// const ctx = document.getElementById('mychart').getContext('2d');
+// const myChart = new CharacterData(ctx, {
+//     type:'line',
+//     data:{
+//         labels: xlabels,
+//         datasets: [
+//             {
+//                 data:ylabels
+//             }
+//         ]
+//     } 
+// }
+// )
 // papa.parse('test.csv')
-
+const Chart = require('chart.js');
+const ctx2 = document.getElementById("myChart2").getContext('2d');
+const myChart2 = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
 
 // function setup(){
