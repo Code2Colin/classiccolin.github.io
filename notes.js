8a19ad5a8332b94cc7d6617fbe5e5a3535c5e2dd
classiccolin <classiccolin@gmail.com>
2023-03-11 13:44:54 -0800

    Added scale lines and clef svg files

--- notes.js
+++ notes.js
@@ -1,30 +1,51 @@
-const pianotes =["A","B","C","D","E","F","G"];
+const pianotes =[["A",[150,220,290]],
+                ["B",[140,210,280]],
+                ["C",[130,200,270]],
+                ["D",[120,190,260]],
+                ["E",[110,180,250]],
+                ["F",[100,170,240,310]],
+                ["G",[90,160,230,300]]];
+let fClef;
+let gClef;
 
-function setup() {
-
-    
-    createCanvas(300,300);
-    frameRate(1/2);
-
+function preLoad(){
+    fClef = loadImage('FClef.svg');
+    gClef = loadImage('GClef.svg');
 }
 
-function mousePressed(){
-   createP(random(pianotes)); 
+function setup() {
+    createCanvas(300,320); //(X,Y)
+    background(220);
+    // randomNote();
+    // frameRate(1/2);
+    image(gClef,0,0);
+
 }
 
 function draw() {
-
     background(220);
-
-    //const notes =["A","B","C","D","E","F","G"];
-
-    let a = random(pianotes);
-    strokeWeight(12);
-
-    textSize(64);
-
-    text(" " + a, 130, 160);
-    //createP(a);
+    // randomNote();
+    // image(gClef,30,100);
 
 }
 
+function randomNote() {
+    let a = random(pianotes);
+    let b = random(a[1]);
+    strokeWeight(2);
+    textSize(60);
+    fill("black");  
+    
+    text(" " + a[0], 110, 70);
+    // rotate(PI/4);
+    ellipse(150,b,26,18);
+    fill(220);
+    ellipse(150,b,16);
+    // rotate(-PI/4);
+    for( let i = 0; i<5; i++) {
+        line(10,100+20*i,290,100+20*i);
+        line(10,220+20*i,290,220+20*i);
+    }
+    // image(gClef,30,100);
+    // image(fClef,30,200);
+}
