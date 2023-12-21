4034d6080aa9610fffd258652e21046ceff4acf8
classiccolin <classiccolin@gmail.com>
2023-12-21 13:04:18 -0800

    used for loop to create button and changed a few variable names

--- notes.js
+++ notes.js
@@ -8,18 +8,10 @@ const pianotes =[["A",[150,220,290]],
 let fClef;
 let gClef;
 let speed=.333;
-let a;
-let b; 
-var noteA;
-var noteB;
-var noteC;
-var noteD;
-var noteE;
-var noteF;
-var noteG;
+let currentNote;
+let notePosition; 
 var score = 0;
 var wrong = 0;
-
 
 
 function preload(){
@@ -28,36 +20,17 @@ function preload(){
 }
 
 function setup() {
-    
     var canvas = createCanvas(300,320); //(X,Y)
     canvas.parent("myCanvas");
     background(230);
     randomNote();
     frameRate(speed);
- 
-    
-    noteA = createButton("A");
-    noteA.mousePressed(checkNote,"A");
-    noteA.parent("myCanvas");
-    noteB = createButton("B");
-    noteB.mousePressed(checkNote,"B");
-    noteB.parent("myCanvas");
-    noteC = createButton("C");
-    noteC.mousePressed(checkNote,"C");
-    noteC.parent("myCanvas");
-    noteD = createButton("D");
-    noteD.mousePressed(checkNote,"D");
-    noteD.parent("myCanvas");
-    noteE = createButton("E");
-    noteE.mousePressed(checkNote,"E");
-    noteE.parent("myCanvas");
-    noteF = createButton("F");
-    noteF.mousePressed(checkNote,"F");
-    noteF.parent("myCanvas");
-    noteG = createButton("G");
-    noteG.mousePressed(checkNote,"G");
-    noteG.parent("myCanvas");
 
+    for (var i = 0; i < pianotes.length; i++){
+        var note = createButton(pianotes[i][0]);
+        note.mousePressed(checkNote, pianotes[i][0]);
+        note.parent("myCanvas");
+    }
 
 
 }
@@ -73,13 +46,13 @@ function mousePressed() {
     strokeWeight(2);
     textSize(60);
     fill("black");  
-    text(" " + a[0], 110, 70);
+    text(" " + currentNote[0], 110, 70);
     // textSize(3);
     // text("Speed "+speed,0,0);
 }
 
 function checkNote(guess) {
-    if (this.elt.innerText == a[0] ) {
+    if (this.elt.innerText == currentNote[0] ) {
         score++;
         console.log(score);
         select('#score').elt.innerText = score
@@ -94,18 +67,18 @@ function checkNote(guess) {
 }
 
 function randomNote() {
-    a = random(pianotes);
-    b = random(a[1]);
+    currentNote = random(pianotes);
+    notePosition = random(currentNote[1]);
     strokeWeight(2);
     textSize(60);
     fill("black");  
-    // text(" " + a[0], 110, 70);
+    // text(" " + currentNote[0], 110, 70);
 
-    ellipse(150,b,28,18);
+    ellipse(150,notePosition,28,18);
     fill(230);
-    ellipse(150,b,18);
-    if(b==200) {
-        line(120,b,180,b);
+    ellipse(150,notePosition,18);
+    if(notePosition==200) {
+        line(120,notePosition,180,notePosition);
     }
 
     for( let i = 0; i<5; i++) {
