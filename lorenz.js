d3f043512a45df2d8ba9fe347d589b2e0bb8fc5e
classiccolin <classiccolin@gmail.com>
2024-02-27 14:08:19 -0800

    Added a timer page with beeps

--- lorenz.js
+++ lorenz.js
@@ -1,28 +1,24 @@
-let head = [1, 1, 1.50]
+let head = [10, 10, 1.50]
 let tail = [0, 0, 0];
-let scaling= 5;
+let scaling= 4;
 
 function setup() {
     let canvas = createCanvas(600,600,WEBGL);
     canvas.parent("myCanvas");
     frameRate(300);
-    // console.log(deltaTime);
     background(175);
 }
 
 function draw() {
-    
     tail = lorenz(head)
     for (let i =0; i<head.length;i++){
         tail[i] = head[i] + tail[i]*1/100;
     }
     line(head[0]*scaling,head[1]*scaling,head[2]*scaling,tail[0]*scaling,tail[1]*scaling,tail[2]*scaling)
-    head= tail
-    // print(head)
-    camera()
+    head = tail
 }
 
-function lorenz(xyz, s=10,r=21,b=8/3){
+function lorenz(xyz, s=10,r=52,b=8/3){
     let x = xyz[0]
     let y = xyz[1]
     let z = xyz[2]
---
+++ timer.js
@@ -0,0 +1,34 @@
+let interval = document.getElementById("interval").value;
+let numberOfSets = document.getElementById("numberOfSets").value;
+var beep = document.getElementById("myaudio");
+
+// resetClock
+function resetClock(){
+    window.location.reload();
+}
+
+function update(curr,inter) {
+    document.getElementById("timer2").innerHTML= curr
+    let current = Number(document.getElementById("timer2").innerHTML);
+        if (current % 60 == 0){
+            beep.play();
+        } else if (current - inter % 60 == 0){
+            beep.play();
+        } else {}
+    }
+// startClock
+function startClock() {
+    // if (document.getElementById("interval20").checked) {
+    //     interval = 20
+    // } else if (document.getElementById("interval40").checked) {
+    //     interval = 40
+    // } else if (document.getElementById("interval60").checked) {
+    //     interval = 60
+    // } else {interval=2};
+    interval = document.getElementById("interval").value;
+    numberOfSets = document.getElementById("numberOfSets").value;
+
+    for(let secs=0; secs<=numberOfSets*60;secs++){
+        setTimeout(()=>update(secs,interval),secs*1000);
+    }
+}
